d57092e3d4492e958082472c05e14068340188be
santosfabin <fsfabianosantos03@gmail.com>
2025-05-28 23:07:24 -0300

    refactore: move back into its own folder

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,15 +3,24 @@ import routes from './routes';
 import pool from './database/connection';
 import config from './config';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 const app: Express = express();
-
 const PORT: number = Number(config.PORT) || 3000;
 
 app.use(cookieParser());
 app.use(express.json());
 
 app.use('/api', routes);
+
+// Serve arquivos estáticos do React (build)
+const staticPath = path.join(__dirname, '../../frontend/dist');
+app.use(express.static(staticPath));
+
+// Redireciona qualquer rota não API para index.html
+app.get(`/{*splat}`, (_req, res) => {
+  res.sendFile(path.join(staticPath, 'index.html'));
+});
 
 pool
   .connect()
